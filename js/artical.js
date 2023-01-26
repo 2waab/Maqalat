0465e69a68a94649a0d40fbe7106184b7b4230df
Awaab Abdelbagi <92119191+2waab@users.noreply.github.com>
2023-01-27 02:44:10 +0300

    Update artical.js

--- js/artical.js
+++ js/artical.js
@@ -6,6 +6,7 @@ let link = document.querySelectorAll("header nav a");
 let content = document.querySelector(".page-of-artical div");
 let title = document.querySelector("title");
 
+
 // Get Information From Local Storge
 let mainData = window.localStorage.getItem("get");
 let dataObj = JSON.parse(mainData);
@@ -15,11 +16,32 @@ for (let i = 0; i < dataObj.length; i++) {
         content.innerHTML = `
             <h3>${dataObj[i].title}</h3>
             <div class="info">
-                <p>${dataObj[i].author}</p>
-                <p>${dataObj[i].bookmark}</p>
+                <i class="fa fa-gear sett"></i>
+                <div> 
+                    <p>${dataObj[i].author}</p>
+                    <p>${dataObj[i].bookmark}</p>
+                </div>
             </div>
             <div class="content">${dataObj[i].content}</div>
         `;
+
+        // Show And Hide Setting Box
+        let btnSett = document.querySelector(".page-of-artical .info i");
+        let overlay = document.querySelector(".overlay");
+        let btnOnBox = document.querySelector(".hide-box");
+        let boxSetting = document.querySelector(".box-setting");
+        btnSett.addEventListener("click", () => {
+            overlay.classList.add("show");
+            boxSetting.classList.add("show");
+        });
+        overlay.onclick = () => {
+            overlay.classList.remove("show");
+            boxSetting.classList.remove("show");
+        };
+        btnOnBox.onclick = () => {
+            overlay.classList.remove("show");
+            boxSetting.classList.remove("show");
+        };
     };
 };
 
@@ -39,4 +61,19 @@ link.forEach((e) => {
     e.onclick = () => {
         nav.classList.remove("open");
     };
+});
+
+// Change font Of Artical 
+let listFont = document.querySelectorAll(".setting-font ul li");
+listFont.forEach(li => {
+    li.addEventListener("click", () => {
+        listFont.forEach(element => {
+            element.classList.remove("active");
+            li.classList.add("active");
+            let allPragraph = document.querySelectorAll(".content p");
+            allPragraph.forEach(p => {
+                p.style.fontSize = `${li.dataset.size}`;
+            });
+        });
+    });
 });
